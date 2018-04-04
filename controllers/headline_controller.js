fe0e8362754a1f601bfefc20d2ae00d1d7250070
swoozeki <sujitkarki95@gmail.com>
2018-04-04 11:26:39 -0400

    top_get and new_get now work as expected

--- controllers/headline_controller.js
+++ controllers/headline_controller.js
@@ -1,2 +1,14 @@
-module.exports.top_get = function (req, res, next) {}
-module.exports.new_get = function (req, res, next) {}
+module.exports.top_get = function (req, res, next) {
+    const {Headline} = req.models;
+
+    Headline.findAll({where: {freshness: 'top'}})
+        .then(headlines => res.json(headlines))
+        .catch(err => next(err));
+}
+module.exports.new_get = function (req, res, next) {
+    const {Headline} = req.models;
+
+    Headline.findAll({where: {freshness: 'new'}})
+        .then(headlines => res.json(headlines))
+        .catch(err => next(err));
+}
