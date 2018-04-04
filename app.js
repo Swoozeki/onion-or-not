8f58fedbd0f3ec1eb7468ed3c2e87fe13e0101c3
swoozeki <sujitkarki95@gmail.com>
2018-04-03 21:06:01 -0400

    configured sequelize and now fetch-new-headlines.js actually saved the fetched headlines to database

--- app.js
+++ app.js
@@ -4,13 +4,20 @@ const app = express();
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('postgres://wjzdssqo:oTdYEcAjKFQo9DlPy_SbiwYRcweTrhwC@stampy.db.elephantsql.com:5432/wjzdssqo', {logging: false});
+
+sequelize.authenticate().then(()=>console.log('database authenticated')).catch(err=>console.log(err));
+const Headline = sequelize.import('./models/Headline.js');
+app.use((req, res, next) => req.models = {Headline});
+
 //setup app
 app.use(logger('tiny'));
 app.use(express.static('./static'));
 app.use(bodyParser.json(), bodyParser.urlencoded({extended:false}));
 
 //helper function to fetch new headlines
-require('./helpers/fetch-new-headlines.js')();
+require('./helpers/fetch-new-headlines.js')(Headline);
 
 //import routes
 require('./routes.js')(app);
--- helpers/fetch-new-headlines.js
+++ helpers/fetch-new-headlines.js
@@ -3,21 +3,28 @@ const axios = require('axios');
 const millisecondsInDay = 86400000;
 const millisecondsInWeek = 604800000;
 
+let HeadlineModel;
 /*
     fetch new headlines every day, and top headlines every week
 */
-module.exports = function () {
+module.exports = function (Headline) {
+    HeadlineModel = Headline;
     const lastNewFetched = getLastFetched('new');
     const lastTopFetched = getLastFetched('top');
     let headlines;
     //in case the last fetched new headlines are over a day old
-    if (Date.now() - lastNewFetched >= millisecondsInDay) {
-        getHeadlines('new');
-    }
+    getLastFetched('new', function(lastNewFetched){
+        if (Date.now() - lastNewFetched >= millisecondsInDay) {
+            getHeadlines('new');
+        }
+    });
+    
     //in case the last fetched top headlines are over a week old
-    if (Date.now() - lastTopFetched >= millisecondsInWeek) {
-        getHeadlines('top');
-    }
+    getLastFetched('top', function(lastTopFetched){
+        if (Date.now() - lastTopFetched >= millisecondsInWeek) {
+            getHeadlines('top');
+        }
+    });
 
     setTimeout(function () { //fetch new headlines every 24 hours
         getHeadlines('new');
@@ -45,21 +52,16 @@ function getHeadlines(freshness) {
             );
 
             pushToDatabase(freshness, headlines);
-
-            //log examples to console
-            const randomNumber = Math.floor(Math.random() * 49);
-            console.log(`${freshness} headlines: ${headlines.length}`);
-            console.log(`example: ${JSON.stringify(headlines[randomNumber].headline)}`);
-            console.log(`reveal: ${JSON.stringify(headlines[randomNumber].subreddit)}`);
-            console.log('================================');
         }))
         .catch(err => {
             throw err;
         });
 }
 
-function getLastFetched(freshness) { //get last fetched from 'createdAt' column on headlines table
-    return 0;
+function getLastFetched(freshness, callback) { //get last fetched from 'createdAt' column on headlines table
+    HeadlineModel.find({where:{freshness: freshness}})
+        .then(headline => callback(new Date(headline.createdAt).getTime()))
+        .catch(err => 0);  
 }
 
 function refinePosts(freshness, responsePosts) {
@@ -69,11 +71,20 @@ function refinePosts(freshness, responsePosts) {
         refinedPosts.push({
             headline: responsePost.title,
             url: responsePost.url,
-            subreddit: responsePost.subreddit,
-            freshness: freshness
+            subreddit: responsePost.subreddit.toLowerCase(),
+            freshness: freshness,
+            votes: {onion: 0, notOnion: 0}
         });
     });
     return refinedPosts;
 }
 
-function pushToDatabase(freshness, headlines) {} //to do
+function pushToDatabase(freshness, headlines) {
+    HeadlineModel.destroy({where: {freshness: freshness}})
+        .then(affectedHeadlines => {
+            console.log(`${affectedHeadlines} ${freshness} headlines were deleted!`);
+            return HeadlineModel.bulkCreate(headlines);
+        })
+        .then(()=>console.log(`created ${headlines.length} ${freshness} headlines`))
+        .catch(err => console.log(err));
+} 
