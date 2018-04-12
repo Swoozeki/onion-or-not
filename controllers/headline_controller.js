86648fa15ca79bc703fa5800e5e6a24979747258
swoozeki <sujitkarki95@gmail.com>
2018-04-12 18:27:42 -0400

    Revert "top_get and new_get now returns a random respective query, whereas the headlines_get controller will get all the headlines in the database"
    This reverts commit f5e86f077f99b0492803074ee583961aabf5dc9c.

--- controllers/headline_controller.js
+++ controllers/headline_controller.js
@@ -2,28 +2,13 @@ module.exports.top_get = function (req, res, next) {
     const {Headline} = req.models;
 
     Headline.findAll({where: {freshness: 'top'}})
-        .then(headlines => {
-            const randomIndex = Math.floor(Math.random()*(headlines.length-1));
-
-            return res.json(headlines[randomIndex]);
-        })
+        .then(headlines => res.json(headlines))
         .catch(err => next(err));
 }
 module.exports.new_get = function (req, res, next) {
     const {Headline} = req.models;
 
     Headline.findAll({where: {freshness: 'new'}})
-        .then(headlines => {
-            const randomIndex = Math.floor(Math.random()*(headlines.length-1));
-
-            return res.json(headlines[randomIndex]);
-        })
-        .catch(err => next(err));
-}
-module.exports.headlines_get = function (req, res, next) {
-    const {Headline} = req.models;
-
-    Headline.findAll()
         .then(headlines => res.json(headlines))
         .catch(err => next(err));
 }
