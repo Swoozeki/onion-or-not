3475e3bc747aae48f103a7afa46e11425f7555ee
swoozeki <sujitkarki95@gmail.com>
2018-04-27 00:43:55 -0400

    added and setup helmet to middleware stack

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+const helmet = require('helmet');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
@@ -10,7 +11,8 @@ const Sequelize = require('sequelize');
     Setup sequelize, and pass the created models to the Request object
     Created models will be available from any middleware at Request.models
 */
-const sequelize = new Sequelize('postgres://wjzdssqo:oTdYEcAjKFQo9DlPy_SbiwYRcweTrhwC@stampy.db.elephantsql.com:5432/wjzdssqo', {logging: false});
+const dbAddress = process.env.PG_URI || 'postgres://wjzdssqo:oTdYEcAjKFQo9DlPy_SbiwYRcweTrhwC@stampy.db.elephantsql.com:5432/wjzdssqo';
+const sequelize = new Sequelize(dbAddress, {logging: false});
 sequelize.authenticate().then(()=>console.log('database authenticated')).catch(err=>console.log(err));
 const Headline = sequelize.import('./models/Headline.js');
 app.use((req, res, next) => {
@@ -20,10 +22,12 @@ app.use((req, res, next) => {
 
 /*
     Setup the app with several useful third-party middlewares
+    Helmet - to protect against well-known vulnerabilities
     Logger - to log http requests to console
     Express Static - to load static content from /static/
     BodyParser - to populate http request body in Request.body to be accessibly from any middleware
 */
+app.use(helmet());
 app.use(logger('tiny'));
 app.use(express.static('./static'));
 app.use(bodyParser.json(), bodyParser.urlencoded({extended:false}));
